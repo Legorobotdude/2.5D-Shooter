97269dfe606f0e49b8b5d9b55bbea7c567690988
Legorobotdude <adityabawankule@gmail.com>
2018-03-30 18:43:29 -0500

    Initial Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+
+	[SerializeField]float horizontalMovementSpeed = 10f;
+	[SerializeField]bool canJump = true;
+	[SerializeField]bool canJumpInAir = true;
+	[SerializeField]float jumpForce = 100f;
+
+	Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandleVerticalMovement();
+		HandleHorizontalMovement();
+	}
+
+    private void HandleHorizontalMovement()
+    {
+        rigidBody.AddForce(new Vector3(horizontalMovementSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0,0));
+    }
+
+    private void HandleVerticalMovement()
+    {
+        if(Input.GetButtonDown("Jump")&&canJump&&canJumpInAir)
+		{
+			rigidBody.AddForce(new Vector3(0,jumpForce*Time.deltaTime,0));
+		}
+		else if(Input.GetButtonDown("Jump")&&canJump)
+		{
+
+		}
+    }
+}
